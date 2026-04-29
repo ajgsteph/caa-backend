5de0e736cf0716777250576b5f593d9036902568
sitsofecode <sitsofesenouvo@gmail.com>
2026-04-29 21:46:33 +0200

    correct do ker file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,25 @@
 
 echo "Running Laravel setup..."
 
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+# Aller dans le dossier Laravel
+cd /var/www
+
+# Générer la clé si pas définie
+php artisan key:generate --force
+
+# Lancer les migrations
+php artisan migrate --force
+
 
 php artisan migrate --force
 
 # Générer la doc uniquement si nécessaire
 php artisan scribe:generate || true
+
+# Démarrer nginx en arrière-plan
+service nginx start
+
+# Démarrer php-fpm au premier plan (garde le container vivant)
+php-fpm -F
 
 echo "Starting services..."
