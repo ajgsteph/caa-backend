dbde3c52394a0b3f8445c7e7ef5d111e6c2a16c6
sitsofecode <sitsofesenouvo@gmail.com>
2026-06-02 10:44:07 +0100

    add url

--- config/scribe.php
+++ config/scribe.php
@@ -31,7 +31,7 @@ return [
 
   // The base URL displayed in the docs.
   // If you're using `laravel` type, you can set this to a dynamic string, like '{{ config("app.tenant_url") }}' to get a dynamic base URL.
-  'base_url' => config('app.url'),
+'base_url' => env('APP_URL', 'https://api.certifa.art'),
 
   // Routes to include in the docs
   'routes' => [
